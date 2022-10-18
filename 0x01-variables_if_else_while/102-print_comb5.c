33cad28d10cae9628a19c993e01effd046302ac0
Retr0-Xan <mayitey69@gmail.com>
2022-10-17 17:35:51 -0700

    hotfix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -30,8 +30,9 @@ putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
 if (num1 == 98 && num2 == 99)
 continue;
-																														putchar(',');																		putcchar(' ')
-											}
+putchar(',');
+putchar(' ')
+}
 								putchar('\n');
 				return (0);
 }
