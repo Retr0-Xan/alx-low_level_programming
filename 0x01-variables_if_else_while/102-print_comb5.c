86e0f7dafd9912e80277bb87216c49e98ba98c8b
Retr0-Xan <mayitey69@gmail.com>
2022-10-18 17:28:04 -0700

    mods

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,9 @@
  *
  */
 int main(void)
+
 {
+
 int num1, num2;
 for (num1 = 0; num1 <= 98; num1++)
 {
@@ -34,6 +36,6 @@ putchar(',');
 putchar(' ');
 }
 putchar('\n');
-				return (0);
+return (0);
 }
 }
